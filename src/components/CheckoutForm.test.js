cc349aaaf1297998eaa7a059f62c35e74c20274b
Kyle <kyle.e.cardwell@gmail.com>
2021-07-17 12:09:00 -0600

    added working tests, completed all MVP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,15 +1,53 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
 
     render( <CheckoutForm />)
+    
+    // screen.debug();
 
-    screen.debug();
+    const header = screen.getByText(/Checkout Form/i);
+
+    expect(header).toBeInTheDocument();
+    expect(header).not.toBeNull();
 
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+
+    render(<CheckoutForm />);
+
+    const firstNameInput = screen.getByLabelText(/first name:/i);
+    const lastNameInput = screen.getByLabelText(/last name:/i);
+    const addressInput = screen.getByLabelText(/address:/i);
+    const cityInput = screen.getByLabelText(/city:/i);
+    const stateInput = screen.getByLabelText(/state:/i);
+    const zipInput = screen.getByLabelText(/zip:/i);
+    const checkoutButton = screen.getByRole('button');
+
+    let woohooMessage = screen.queryByTestId("successMessage")
+    expect(woohooMessage).toBeNull();
+    
+    userEvent.type(firstNameInput, "Leia");
+    userEvent.type(lastNameInput, "Organa");
+    userEvent.type(addressInput, "Cell 2187");
+    userEvent.type(cityInput, "Detention Level");
+    userEvent.type(stateInput, "Death Star");
+    userEvent.type(zipInput, "00001");
+    userEvent.click(checkoutButton);
+
+    woohooMessage = screen.getByTestId("successMessage");
+
+    const fullName = screen.getByText(/leia organa/i);
+    const cityStateZip = screen.getByText(/Detention Level, Death Star 00001/i);
+
+    expect(woohooMessage).toBeInTheDocument();
+    expect(fullName).toBeInTheDocument();
+    expect(cityStateZip).toBeInTheDocument();
+
+});
